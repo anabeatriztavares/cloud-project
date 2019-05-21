b4d8080071ba221eecb921f2e965efbb66e7e34b
anabeatriztavares <50667794+anabeatriztavares@users.noreply.github.com>
2019-05-21 12:31:48 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,51 @@
 provider "scaleway" {
-  region = "${var.region}"
-  organization = "${var.org}"
-  token = "${var.tok}"
+  organization = "4be407ab-26b3-44bc-b553-8219824f3c68"
+  token        = "bf445b02-e3e0-44a2-a044-c1bb142af8e1"
+  region       = "ams1"
 }
 
-data "scaleway_image" "terraformana" {
-  architecture = "x86_64"
-  name         = "Ubuntu Bionic"
-}
-
-resource "scaleway_server" "terraformana" {
-  name  = "terraformana"
-  image = "${data.scaleway_image.terraformana.id}"
-  type  = "START1-S"
-}
 resource "scaleway_ip" "ip" {
-  server = "${scaleway_server.terraformana.id}"
+  server = "${scaleway_server.test.id}"
 }
 
+resource "scaleway_server" "test" {
+  name  = "test"
+  image = "aecaed73-51a5-4439-a127-6d8229847145"
+  type  = "C2S"
+}
 
-resource "scaleway_volume" "terraformana" {
-  name       = "terraformanavolume"
+resource "scaleway_volume" "test" {
+  name       = "test"
   size_in_gb = 50
   type       = "l_ssd"
+}
+
+resource "scaleway_volume_attachment" "test" {
+  server = "${scaleway_server.test.id}"
+  volume = "${scaleway_volume.test.id}"
+}
+
+resource "scaleway_security_group" "http" {
+  name        = "http"
+  description = "allow HTTP and HTTPS traffic"
+}
+
+resource "scaleway_security_group_rule" "http_accept" {
+  security_group = "${scaleway_security_group.http.id}"
+
+  action    = "accept"
+  direction = "inbound"
+  ip_range  = "0.0.0.0/0"
+  protocol  = "TCP"
+  port      = 80
+}
+
+resource "scaleway_security_group_rule" "https_accept" {
+  security_group = "${scaleway_security_group.http.id}"
+
+  action    = "accept"
+  direction = "inbound"
+  ip_range  = "0.0.0.0/0"
+  protocol  = "TCP"
+  port      = 443
 }
