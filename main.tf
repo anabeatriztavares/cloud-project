2cbba002bf1c3f5eb899ae226038cb5112bfd6fa
anabeatriztavares <50667794+anabeatriztavares@users.noreply.github.com>
2019-05-21 12:17:26 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,26 @@
+provider "scaleway" {
+  region = "${var.region}"
+  organization = "${var.org}"
+  token = "${var.tok}"
+}
 
+data "scaleway_image" "terraformana" {
+  architecture = "x86_64"
+  name         = "Ubuntu Bionic"
+}
+
+resource "scaleway_server" "terraformana" {
+  name  = "terraformana"
+  image = "${data.scaleway_image.terraformana.id}"
+  type  = "START1-S"
+}
+resource "scaleway_ip" "ip" {
+  server = "${scaleway_server.terraformana.id}"
+}
+
+
+resource "scaleway_volume" "terraformana" {
+  name       = "terraformanavolume"
+  size_in_gb = 50
+  type       = "l_ssd"
+}
